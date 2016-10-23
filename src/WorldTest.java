f9c36ee8995d8390a5bd4a9a9eb55ea50dfd5606
songleeblr <1026271646@qq.com>
2016-10-23 15:37:04 +0800

    tijiao

---
+++ src/WorldTest.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class WorldTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testRun() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPaintComponentGraphics() {
+		fail("Not yet implemented");
+	}
+
+}
